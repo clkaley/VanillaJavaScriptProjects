22f8e77813f1a32502a9e3cb7c1966164c0d0284
clkaley <clkkaley@gmail.com>
2021-06-24 16:25:04 +0300

    commit

---
+++ Counter/app.js
@@ -0,0 +1,48 @@
+//set initial count (başlangıç sayısını ayarlama)
+
+let count=0;
+
+//select value and buttons (butonları ve değerleri seç)
+//const decBTN=document.getElementByClass("decrease");
+//const resetBTN=document.getElementByClass("reset");
+//const incBTN=document.getElementByClass("increase");
+const btns=document.querySelectorAll('.btn');
+const value=document.getElementById('value');
+
+//btns.forEach(function(item){
+ //   console.log(item)
+//});
+
+
+btns.forEach(function(item){
+  item.addEventListener('click',function(e){
+     // console.log(e.currentTarget);
+    const style=e.currentTarget.classList;
+    if(style.contains('decrease')){
+        count--;
+    }
+
+    else if(style.contains('increase')){
+        count++;
+    }
+    else {
+        count=0;
+    }
+
+    if(count>0){
+        value.style.color='green';
+    }
+
+    if(count<0){
+        value.style.color="red";
+    }
+
+    value.textContent=count;
+
+
+  })
+   });
+
+
+
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Color Flipper
 
+
+### Counter
